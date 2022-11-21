f01030905c3c8b1afd644f80f59c1207cabd332c
muhammadhanif71 <118676379+muhammadhanif71@users.noreply.github.com>
2022-11-21 20:18:59 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+from sample_config import Config
+
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 10687268
+    API_HASH = "6a6cc00a8c9721f1de6516f555dbd511"
+    # the name to display in your alive message
+    ALIVE_NAME = "Dragons-Userbot"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "postgres://diwqyuwf:75u9gtu13qgSB0ATWOD6tWLqET874xvr@tiny.db.elephantsql.com/diwqyuwf"
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value>
+    STRING_SESSION = "1BVtsOL8Bu5_iYPN5asRt3eH8MkKEU2tEirq5-iL4LngVNG9d8NOUGQ2L4CrZyaM5AtT2QJ0C1EWc>
+    # create a new bot in @botfather and fill the following vales with bottoken
+    TG_BOT_TOKEN = "5461018320:AAFEzJqqPbRau1wVZ6jPEmrtwFXeaq5Ku1c"
+    # create a private group and a rose bot to it and type /id and paste that id here (replace that>
+    PRIVATE_GROUP_BOT_API_ID = -1001556560624
+    # command handler
+    COMMAND_HAND_LER = "$"
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
+ 
